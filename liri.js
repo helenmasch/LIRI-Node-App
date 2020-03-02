45954cf21a1dd4c1925a4f94b7901fdb34c83503
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-03-01 21:21:24 -0500

    added spotify variable

--- liri.js
+++ liri.js
@@ -2,3 +2,16 @@ require("dotenv").config();
 
 var keys = require("./keys");
 var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify({
+    id: <your spotify client id>,
+    secret: <your spotify client secret>
+  });
+   
+  spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
