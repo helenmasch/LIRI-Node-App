c09805b0cbc2453cdccb013039352519a00c2969
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-03-01 23:11:22 -0500

    added functions to spotify-bandsintown

--- liri.js
+++ liri.js
@@ -3,15 +3,98 @@ require("dotenv").config();
 var keys = require("./keys");
 var Spotify = require('node-spotify-api');
 
-var spotify = new Spotify({
-    id: <your spotify client id>,
-    secret: <your spotify client secret>
-  });
-   
-  spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+
+var axios = require("axios");
+
+var command=process.argv[2]
+var keyword=process.argv.slice(3).join(" ")
+ 
+startSearch()
+ 
+function startSearch() {
+    if (command=== "concert-this") {
+        searchConcert()
     }
+    else if (command=== "spotify-this-song") {
+        searchSpotify()
+    }
+
+    else if (command=== "movie-this") {
+        searchMovie()
+    }
+
+    else if (command=== "do-what-it-says") {
+        searchAnything()
+    }
+} 
+// concert-this
+
+// spotify-this-song
+
+// movie-this
+
+// do-what-it-says
+
+function searchConcert() {
+ axios.get("https://rest.bandsintown.com/artists/" + keyword + "/events?app_id=codingbootcamp").then(function(response){
    
-  console.log(data); 
-  });
+    // Name of the venue
+
+    // Venue location
+    
+    // Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+     var concertList = response.data
+     for (let i = 0; i < concertList.length; i++) {
+     console.log("venue", concertList[i].venue.name)
+     console.log("city", concertList[i].venue.city)
+     console.log("country", concertList[i].venue.country)
+     console.log("date", concertList[i].datetime)
+     }
+ })
+}
+
+function searchSpotify() {
+    var spotify = new Spotify(keys.spotify);
+   
+ 
+    // Artist(s)
+
+    // The song's name
+    
+    // A preview link of the song from Spotify
+    
+    // The album that the song is from
+
+
+
+    spotify.search({ type: 'track', query: keyword }, function(err, response) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+     
+    console.log(response.tracks.items); 
+       var songList = response.tracks.items
+       for (let i = 0; i < songList.length; i++) {
+          
+             console.log("artists:", songList[i].album.artists[0].name) 
+             console.log("songs", songList[i].name)
+             console.log("preview link", songList[i].preview_url)
+             console.log("album name", songList[i].album.name)
+             console.log("________________________________\n")
+       }
+
+    });
+}
+
+function searchMovie() {
+
+}
+
+function searchAnything() {
+
+}
+
+
+
+  console.log(process.argv);
